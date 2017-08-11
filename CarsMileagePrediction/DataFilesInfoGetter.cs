681ffb0be526f9085eb3474ca7c0393be6413427
Przemysław Owsianik <Przemek.Owsianik@yahoo.com>
2017-08-11 11:56:37 +0200

    Add shuffle data step

---
+++ CarsMileagePrediction/DataFilesInfoGetter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace CarsMileagePrediction
+{
+    public class DataFilesInfoGetter
+    {
+        public static FileInfo BasePath = new FileInfo($@"{Directory.GetCurrentDirectory().Replace(@"CarsMileagePrediction\bin\Debug", string.Empty)}Data\");
+
+        public static FileInfo BaseFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG.csv"));
+
+        public static FileInfo ShuffledBaseFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG_Shuffled.csv"));
+
+        public static FileInfo TrainingFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG_Training.csv"));
+
+        public static FileInfo EvaluateFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG_Evaluate.csv"));
+
+        public static FileInfo NormalizedTrainingFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG_NormalizedTraining.csv"));
+
+        public static FileInfo NormalizedEvaluateFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG_NormalizedEvaluate.csv"));
+
+        public static FileInfo EncogAnalystFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG_EncogAnalyst.ega"));
+
+        public static FileInfo NetworkFile = new FileInfo(Path.Combine(BasePath.DirectoryName, "AutoMPG_Network.eg"));
+    }
+}
--- CarsMileagePrediction/Program.cs
+++ CarsMileagePrediction/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace CarsMileagePrediction
+﻿using System;
+using CarsMileagePrediction.Steps;
+
+namespace CarsMileagePrediction
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Step1();
+        }
 
+        static void Step1()
+        {
+            Console.WriteLine("STEP 1: Shuffle data...");
+            DataShuffle shuffle = new DataShuffle();
+            shuffle.Shuffle(DataFilesInfoGetter.BaseFile, DataFilesInfoGetter.ShuffledBaseFile, true);
         }
     }
 }
