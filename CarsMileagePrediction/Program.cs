1ebd64def15f074f43086495c26d2179958197c8
Przemysław Owsianik <Przemek.Owsianik@yahoo.com>
2017-08-11 11:59:16 +0200

    Added data segregation step

--- CarsMileagePrediction/Program.cs
+++ CarsMileagePrediction/Program.cs
@@ -8,6 +8,7 @@ namespace CarsMileagePrediction
         static void Main(string[] args)
         {
             Step1();
+            Step2();
         }
 
         static void Step1()
@@ -15,6 +16,13 @@ namespace CarsMileagePrediction
             Console.WriteLine("STEP 1: Shuffle data...");
             DataShuffle shuffle = new DataShuffle();
             shuffle.Shuffle(DataFilesInfoGetter.BaseFile, DataFilesInfoGetter.ShuffledBaseFile, true);
+        }
+
+        static void Step2()
+        {
+            Console.WriteLine("STEP 2: Segregate data...");
+            DataSegregator segregator = new DataSegregator(75, 25);
+            segregator.Segregate(DataFilesInfoGetter.ShuffledBaseFile, DataFilesInfoGetter.TrainingFile, DataFilesInfoGetter.EvaluateFile);
         }
     }
 }
