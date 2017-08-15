d9a9d1091dfe50883462d3519699a62b3329b569
Przemysław Owsianik <Przemek.Owsianik@yahoo.com>
2017-08-15 12:17:03 +0200

    Add training step

--- CarsMileagePrediction/Program.cs
+++ CarsMileagePrediction/Program.cs
@@ -11,6 +11,7 @@ namespace CarsMileagePrediction
             Step2();
             Step3();
             Step4();
+            Step5();
 
             Console.ReadKey();
         }
@@ -42,6 +43,13 @@ namespace CarsMileagePrediction
             Console.WriteLine("STEP 4: Create neural network...");
             NetworkCreator networkCreator = new NetworkCreator();
             networkCreator.Create(DataFilesInfoGetter.NetworkFile);
+        }
+
+        static void Step5()
+        {
+            Console.WriteLine("STEP 5: Train neural network...");
+            NetworkTrainer networkteTrainer = new NetworkTrainer();
+            networkteTrainer.Train(DataFilesInfoGetter.NetworkFile, DataFilesInfoGetter.NormalizedTrainingFile);
         }
     }
 }
